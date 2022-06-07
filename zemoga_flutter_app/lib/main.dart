4b7a3d2c2f028b974e7dae190484325b2d28b086
juancano5304 <juancano5304@gmail.com>
2022-06-06 22:52:40 -0500

    Feat: add settings.json file

--- zemoga_flutter_app/lib/main.dart
+++ zemoga_flutter_app/lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Material App Bar'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
